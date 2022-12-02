92e97581cb5b3a9144f28b3ad8f2078c52d3ee33
solarix95 <rheld@gmx.net>
2022-12-02 20:18:20 +0100

    Qt Kompat < 5.10

--- libqtr3d/utils/qtr3dutils.cc
+++ libqtr3d/utils/qtr3dutils.cc
@@ -1,12 +1,25 @@
+
 #include <QThread>
 #include <QCoreApplication>
+
+#if QT_VERSION < QT_VERSION_CHECK(5, 10, 0)
+#include <QtGlobal>
+#else
 #include <QRandomGenerator>
+#endif
+
 #include "qtr3dutils.h"
 
 
 qint32 Qtr3d::qrand()
 {
     Q_ASSERT(qApp->thread() == QThread::currentThread());
+
+#if QT_VERSION < QT_VERSION_CHECK(5, 10, 0)
+    return qrand();
+#else
     return QRandomGenerator::global()->generate();
+#endif
+
 
 }
